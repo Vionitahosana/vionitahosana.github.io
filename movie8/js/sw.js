55e6d04030d7ddb4032c61d380c807660d61c458
Vionitahosana <vionitahosana1203@gmail.com>
2021-11-22 23:29:27 +0800

    update tugas prak 8 lanjutan

--- movie8/js/sw.js
+++ movie8/js/sw.js
@@ -1,51 +1,55 @@
 const CACHE_NAME = "pwebmovie";
 var urlsToCache = [
-"/",
-"/js/tmdb.js",
-"/js/materialize.min.js",
-"/js/init.js",
-"/js/api.js",
-"/css/materialize.min.css",
-"/img/1080.jpg",
-"/img/720.jpg",
-"/img/480.jpg",
-"/css/style.css ",
+    "/",
+    "/js/tmdb.js",
+    "/js/materialize.min.js",
+    "/js/init.js",
+    "/js/api.js",
+    "/css/materialize.min.css",
+    "/img/1080.jpg",
+    "/img/720.jpg",
+    "/img/480.jpg",
+    "/css/style.css ",
 ];
+
 self.addEventListener("install", function(event) {
-event.waitUntil(
-caches.open(CACHE_NAME).then(function(cache) {
-return cache.addAll(urlsToCache);
-})
-);
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
+
 self.addEventListener("fetch", function(event) {
     event.respondWith(
     caches
     .match(event.request, { cacheName: CACHE_NAME })
     .then(function(response) {
-    if (response) {
-    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-    return response;
-}
-console.log(
-"ServiceWorker: Memuat aset dari server: ",
-event.request.url
-);
-return fetch(event.request);
-})
-);
+        if (response) {
+            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+            return response;
+        }
+        
+        console.log(
+            "ServiceWorker: Memuat aset dari server: ",
+            event.request.url
+        );
+        return fetch(event.request);
+    })
+    );
 });
+
 self.addEventListener("activate", function(event) {
-event.waitUntil(
-caches.keys().then(function(cacheNames) {
-return Promise.all(
-cacheNames.map(function(cacheName) {
-if (cacheName != CACHE_NAME) {
-console.log("ServiceWorker: cache " + cacheName + " dihapus");
-return caches.delete(cacheName);
-}
-})
-);
-})
-);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
